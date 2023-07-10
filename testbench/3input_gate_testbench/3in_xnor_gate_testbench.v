3385eb087559c2e4bad578c968940a583fb20d9f
흐이수 <132325298+galahaed@users.noreply.github.com>
2023-07-10 16:27:50 +0900

    Add files via upload

---
+++ testbench/3input_gate_testbench/3in_xnor_gate_testbench.v
@@ -0,0 +1,42 @@
+module testbench();
+
+reg a, b, c;
+wire out;
+
+xnor_gate_3input ins_xnor_gate_3(
+    .a(a),
+    .b(b),
+    .c(c),
+    .F(out)
+);
+
+initial 
+begin
+    #0;
+    a = 0; b = 0; c = 0;
+
+    #4;
+    a = 0; b = 0; c = 1;
+    
+    #4;
+    a = 0; b = 1; c = 0;
+    
+    #4;
+    a = 0; b = 1; c = 1;
+
+    #4;
+    a = 1; b = 0; c = 0;
+
+    #4;
+    a = 1; b = 0; c = 1;
+
+    #4;
+    a = 1; b = 1; c = 0;
+
+    #4;
+    a = 1; b = 1; c = 1;
+
+    #4 $stop;
+
+end
+endmodule
---
+++ testbench/3input_gate_testbench/3in_xor_gate_testbench.v
@@ -0,0 +1,42 @@
+module testbench();
+
+reg a, b, c;
+wire out;
+
+xor_gate_3input ins_xor_gate_3(
+    .a(a),
+    .b(b),
+    .c(c),
+    .F(out)
+);
+
+initial 
+begin
+    #0;
+    a = 0; b = 0; c = 0;
+
+    #4;
+    a = 0; b = 0; c = 1;
+    
+    #4;
+    a = 0; b = 1; c = 0;
+    
+    #4;
+    a = 0; b = 1; c = 1;
+
+    #4;
+    a = 1; b = 0; c = 0;
+
+    #4;
+    a = 1; b = 0; c = 1;
+
+    #4;
+    a = 1; b = 1; c = 0;
+
+    #4;
+    a = 1; b = 1; c = 1;
+
+    #4 $stop;
+
+end
+endmodule
